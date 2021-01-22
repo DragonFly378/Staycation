4f50c4328f801da38094c35d4559b82849444342
DargonFly378 <hafichever@gmail.com>
2021-01-22 07:07:09 +0700

    update all

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,10 @@
+import { Component } from "react";
+import React, { Component } from "react";
+
+import modulName from "elements/Button";
+
+function LandingPage() {
+  return <div></div>;
+}
+
+export default LandingPage;
